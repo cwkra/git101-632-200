6e8433a4447f643e56f05853c8a041e2297408a7
cwkra <chawalkorn.ra@ku.th>
2021-02-22 11:09:08 +0700

    add main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+struct data_hero_character {
+    char name[30];
+    char alignment[30];
+    int intelligence,strength,speed,durability,power,combat,total;
+} data_hero_character;
+
+int main() {
+  printf("File Processing in C\n");
+}
